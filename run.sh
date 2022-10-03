37d3e8beefc3ccdaf1455534a26a189760a2ee94
luongngocvinha2 <102142257+luongngocvinha2@users.noreply.github.com>
2022-10-03 21:24:27 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ read -p "  Vui Lòng Nhập : " num
     case "${num}" in
         1) bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
         ;;
-        2) bash <(curl -Ls https://raw.githubusercontent.com/Khiemkhiem123/script/main/config.sh)
+        2) bash <(curl -Ls https://raw.githubusercontent.com/luongngocvinha2/vip-one-stack/main/config.sh)
         ;;
-        *) bash <(curl -Ls https://raw.githubusercontent.com/Khiemkhiem123/script/main/run.sh)
+        *) bash <(curl -Ls https://raw.githubusercontent.com/luongngocvinha2/vip-one-stack/main/run.sh)
         ;;
     esac
